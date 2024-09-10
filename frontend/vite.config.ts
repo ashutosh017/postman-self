65b809595eccffe5c98549236eaaabfc355cf6f3
ashutosh017 <wwwashutosh225@gmail.com>
2024-09-10 20:37:09 +0530

    vite proxy set 2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
-      '/api':"https://postman-self.vercel.app"
-      
+      '/api':"https://postman-self-backend.vercel.app/"
+
     }
   }
 })
